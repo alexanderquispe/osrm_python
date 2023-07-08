806351ac0a6a0db6a690a3ee23720bee0b799923
jhon <fr.jhonk@gmail.com>
2023-07-08 13:24:38 -0500

    fix radius filter

--- osrmareas/areas.py
+++ osrmareas/areas.py
@@ -77,8 +77,7 @@ def get_routes(
 			pd.concat([all_routes_df, get_osrm_route(center1, final1)])
 	
 	if filter_km:
-		rows_inside = all_routes_df['dist_driving_km']  < radius_km
-		all_routes_df_crop = all_routes_df.loc[rows_inside]
+		all_routes_df_crop = all_routes_df.query('dist_driving_km <= @radius_km')
 		return all_routes_df_crop
 	
 	return all_routes_df
