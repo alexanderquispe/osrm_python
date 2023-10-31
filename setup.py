05a56c4abdcaa6051d29a1463405ba225767496b
jhon <fr.jhonk@gmail.com>
2023-10-30 19:29:25 -0500

    add request, error if not instaled

--- setup.py
+++ setup.py
@@ -3,24 +3,25 @@ from setuptools import setup, find_packages
 from osrmareas.version import __version__ as version
 
 setup(
-    name='osrmareas',
+    name="osrmareas",
     version=version,
-    description='Routes area with osrm API',
-    url='https://github.com/d2cml-ai/drdid',
-    author='Jhon Flores',
+    description="Routes area with osrm API",
+    url="https://github.com/d2cml-ai/drdid",
+    author="Jhon Flores",
     license="MIT",
-    author_email='fr.jhonk@gmail.com',
-    packages=['osrmareas'],
+    author_email="fr.jhonk@gmail.com",
+    packages=["osrmareas"],
     install_requires=[
-        'numpy<=1.24.3',
-        'pandas',
-		'geopandas',
-		'alphashape',
-		'shapely',
-        'tqdm',
-        'geopy'
+        "numpy<=1.24.3",
+        "pandas",
+        "geopandas",
+        "alphashape",
+        "shapely",
+        "tqdm",
+        "geopy",
+        "requests",
     ],
-    long_description='''
+    long_description="""
     Using OSRM to create areas and routes within a radius.
-    '''
+    """,
 )
